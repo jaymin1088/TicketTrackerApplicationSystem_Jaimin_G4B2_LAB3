9bb675ed673fa267e4605fba295a5a9b7f26d977
jaymin1088 <139453693+jaymin1088@users.noreply.github.com>
2023-07-14 19:31:26 +0530

    upload

--- Ticket-Tracker-Application-System-Using-Spring-Boot/src/main/java/ErrorrHandlerController/ErrorrHandlerController.java
+++ Ticket-Tracker-Application-System-Using-Spring-Boot/src/main/java/ErrorrHandlerController/ErrorrHandlerController.java
@@ -5,13 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ErrorrHandlerController implements ErrorController{
+public class ErrorrHandlerController implements ErrorController {
 	@GetMapping("/error")
 	public String customError() {
 		return "The link you followed may be broken, or the page may have been removed.";
 	}
 
-	
 	public String getErrorPath() {
 		return "/error";
 	}
